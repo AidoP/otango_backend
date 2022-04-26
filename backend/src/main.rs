af79426312da0782299dc80cd819bd569b736f91
AidoP <aidan@trifuse.xyz>
2022-04-26 22:55:20 +0800

    Proper backend runtime configuration

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,8 @@
-use actix_web::{error, Error, get, web, App, HttpResponse, HttpServer, Responder, HttpRequest, http::header::ContentType, error::ErrorInternalServerError};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+
+use actix_web::{error, Error, get, web, App, HttpResponse, HttpServer, Responder, http::header::ContentType};
 use actix_cors::Cors;
+use once_cell::sync::OnceCell;
 use openssl::ssl::{SslAcceptor, SslMethod, SslFiletype};
 
 type Pool = r2d2::Pool<SqliteConnectionManager>;
@@ -7,24 +10,50 @@ use r2d2_sqlite::SqliteConnectionManager;
 
 use common::data;
 
-const FRONTEND_LOCATION: Option<&'static str> = option_env!("FRONTEND_LOCATION");
+#[derive(Debug, config::Config)]
+struct Config {
+    address: SocketAddr,
+    key: String,
+    cert: String,
+    database: String,
+    root_redirection: Option<String>,
+    allowed_origins: Vec<String>
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            address: SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 443)),
+            key: "key.pem".into(),
+            cert: "cert.pem".into(),
+            database: "お単語.db".into(),
+            root_redirection: None,
+            allowed_origins: vec![]
+        }
+    }
+}
+
+static CONFIG: OnceCell<Config> = OnceCell::new();
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut ssl = SslAcceptor::mozilla_modern_v5(SslMethod::tls())?;
-    ssl.set_private_key_file("key.pem", SslFiletype::PEM)?;
-    ssl.set_certificate_chain_file("cert.pem")?;
+    let config = CONFIG.get_or_init(|| <Config as config::Config>::load("otango"));
 
-    let manager = SqliteConnectionManager::file("お単語.db");
+    let mut ssl = SslAcceptor::mozilla_modern_v5(SslMethod::tls())?;
+    ssl.set_private_key_file(&config.key, SslFiletype::PEM)?;
+    ssl.set_certificate_chain_file(&config.cert)?;
+
+    let manager = SqliteConnectionManager::file(&config.database);
     let pool = Pool::new(manager).unwrap();
 
     HttpServer::new(move || {
-        let cors = Cors::default();
-        let cors = if let Some(location) = FRONTEND_LOCATION {
-            cors.allowed_origin(location)
+        let mut cors = Cors::default();
+        if config.allowed_origins.is_empty() {
+            cors = cors.allow_any_origin();
         } else {
-            cors.allow_any_origin()
-        };
+            for origin in &config.allowed_origins {
+                cors = cors.allowed_origin(&origin);
+            }
+        }
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .wrap(cors)
@@ -33,16 +62,16 @@ async fn main() -> std::io::Result<()> {
             .service(kanji)
             //.service(all_words)
             //.service(backup)
-    }).bind_openssl(("0.0.0.0", 8000), ssl)?
+    }).bind_openssl(config.address, ssl)?
         .run()
         .await
 }
 
 #[get("/")]
 async fn index() -> impl Responder {
-    if let Some(location) = FRONTEND_LOCATION {
+    if let Some(location) = CONFIG.get().and_then(|c| c.root_redirection.as_ref()) {
         HttpResponse::PermanentRedirect()
-            .append_header(("Location", location))
+            .append_header(("Location", location.as_str()))
             .finish()
     } else {
         HttpResponse::NotFound()
